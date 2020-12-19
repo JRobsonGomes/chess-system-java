91cf1605c1a99ba2952b7c4ac73923a38eb99d67
Robson <robsong200@gmail.com>
2020-12-19 16:43:55 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        ChessMatch chessMath = new ChessMatch();
-        UI.printBoard(chessMath.getPieces());
+    	
+    	Scanner sc = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch();
+        
+        while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Origem: ");
+			ChessPosition source = UI.readChessPosition(sc);
+
+			System.out.println();
+			System.out.print("Destino: ");
+			ChessPosition target = UI.readChessPosition(sc);
+
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
     }
 }

86787393f8e4557d42ae0bd6762991fa35c0d9f7
JRobsonGomes <robsong200@gmail.com>
2020-12-24 11:42:26 -0300

    Fix - better treatment to promotion

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -18,21 +18,21 @@ public class Board {
 
     public Piece piece(int row, int column) {        
 		if (!positionExists(row, column)) {
-			throw new BoardException("A posição não está no tabuleiro");
+			throw new BoardException("A posicao nao esta no tabuleiro");
 		}
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
         if (!positionExists(position)) {
-			throw new BoardException("A posição não está no tabuleiro");
+			throw new BoardException("A posicao nao esta no tabuleiro");
 		}
         return pieces[position.getRow()][position.getColumn()];
     }
     
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
-			throw new BoardException("Já existe uma peça na posição " + position);
+			throw new BoardException("Ja existe uma peca na posicao " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
@@ -40,7 +40,7 @@ public class Board {
 
 	public Piece removePiece(Position position) {
 		if (!positionExists(position)) {
-			throw new BoardException("A posição não está no tabuleiro");
+			throw new BoardException("A posicao nao esta no tabuleiro");
 		}
 		if (piece(position) == null) {
 			return null;
@@ -61,7 +61,7 @@ public class Board {
 
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
-			throw new BoardException("A posição não está no tabuleiro");
+			throw new BoardException("A posicao nao esta no tabuleiro");
 		}
 		return piece(position) != null;
 	}
